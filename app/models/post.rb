de7695e42fcd0f775f80b2cb889a91a4a939126e
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-02-27 11:47:33 -0800

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+  validate :is_clickbait?
+
+  def is_clickbait?
+    if self.title.present? && ["Won't Believe", "Secret", "Top [number]", "Guess"].none? {|item| self.title.include?(item)}
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
